28176d945460dc106f7201ef9808b8b91438bb85
zhuy-nguyenz <graio911@gmail.com>
2021-06-09 11:42:29 +0700

    Write Only

---
+++ src/com/codegym/Test.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class Test {
+
+    public static void main(String[] args) {
+	Student student1 = new Student();
+	student1.setName("Satan");
+	student1.setClasses("C666");
+	Student student2= new Student();
+    }
+}
